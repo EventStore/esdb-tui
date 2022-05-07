ea6ea8459f72a6227ec3f1ee35424a7bf5d7c95d
YoEight <yo.eight@gmail.com>
2022-05-08 00:11:08 +0200

    Have a working tab menu.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,38 @@ use std::{
 };
 use tui::{
     backend::{Backend, CrosstermBackend},
-    widgets::Block,
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
     widgets::Borders,
+    widgets::{Block, Tabs},
     Frame, Terminal,
 };
+
+struct App<'a> {
+    pub titles: Vec<&'a str>,
+    pub index: usize,
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            titles: vec!["Dashboard", "Streams"],
+            index: 0,
+        }
+    }
+
+    fn next_tab(&mut self) {
+        self.index = (self.index + 1) % self.titles.len();
+    }
+
+    fn previous_tab(&mut self) {
+        if self.index > 0 {
+            self.index -= 1;
+        } else {
+            self.index = self.titles.len() - 1;
+        }
+    }
+}
 
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
@@ -21,8 +49,8 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     let tick_rate = Duration::from_millis(250);
-
-    let res = run_app(&mut terminal, tick_rate);
+    let app = App::new();
+    let res = run_app(&mut terminal, app, tick_rate);
 
     disable_raw_mode()?;
     execute!(
@@ -39,11 +67,15 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, tick_rate: Duration) -> io::Result<()> {
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    tick_rate: Duration,
+) -> io::Result<()> {
     let mut last_tick = Instant::now();
 
     loop {
-        terminal.draw(|f| ui(f))?;
+        terminal.draw(|f| ui(f, &app))?;
 
         let timeout = tick_rate
             .checked_sub(last_tick.elapsed())
@@ -51,8 +83,11 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, tick_rate: Duration) -> io::R
 
         if crossterm::event::poll(timeout)? {
             if let Event::Key(key) = crossterm::event::read()? {
-                if let KeyCode::Char('q') = key.code {
-                    return Ok(());
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Right => app.next_tab(),
+                    KeyCode::Left => app.previous_tab(),
+                    _ => {}
                 }
             }
         }
@@ -63,10 +98,32 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, tick_rate: Duration) -> io::R
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let size = f.size();
-    let block = Block::default()
-        .title("EventStoreDB Administration Tool")
-        .borders(Borders::ALL);
-    f.render_widget(block, size);
+    // let block = Block::default()
+    //     .title("EventStoreDB Administration Tool")
+    //     .borders(Borders::ALL);
+    // f.render_widget(block, size);
+
+    let titles = app
+        .titles
+        .iter()
+        .map(|t| Spans::from(vec![Span::styled(*t, Style::default().fg(Color::Green))]))
+        .collect();
+
+    let tabs = Tabs::new(titles)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title("EventStoreDB Administration Tool"),
+        )
+        .select(app.index)
+        .style(Style::default().fg(Color::Cyan))
+        .highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .bg(Color::Black),
+        );
+
+    f.render_widget(tabs, size);
 }
