c385b7ecc39e0c4d232ffd921512b756d423b0f2
YoEight <yo.eight@gmail.com>
2022-05-12 22:52:36 +0200

    Parse stats queue structure.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use eventstore::{ClientSettings, StreamPosition};
+use futures::{SinkExt, StreamExt};
+use std::collections::HashMap;
+use std::thread::JoinHandle;
 use std::{
     io,
     time::{Duration, Instant},
 };
 use structopt::StructOpt;
+use tokio::runtime::Runtime;
 use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Layout},
@@ -26,8 +31,8 @@ struct Args {
 
 fn parse_connection_string(
     input: &str,
-) -> std::result::Result<eventstore::ClientSettings, eventstore::ClientSettingsParseError> {
-    eventstore::ClientSettings::parse_str(input)
+) -> Result<ClientSettings, eventstore::ClientSettingsParseError> {
+    ClientSettings::parse_str(input)
 }
 
 struct App<'a> {
@@ -75,8 +80,13 @@ impl<'a> App<'a> {
 
 fn main() -> Result<(), io::Error> {
     let args = Args::from_args();
-    let op_client = eventstore::operations::Client::new(args.conn_setts.clone());
-    let client = eventstore::Client::new(args.conn_setts).unwrap();
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+    let (sender, recv) = futures::channel::mpsc::unbounded();
+    let _ = runtime.spawn(ticking_loop(sender.clone()));
+    let _handle = runtime.spawn(main_esdb_loop(args.conn_setts, recv));
+
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
@@ -84,7 +94,7 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
     let tick_rate = Duration::from_millis(250);
     let app = App::new();
-    let res = run_app(&mut terminal, app, tick_rate);
+    let res = run_app(&runtime, &sender, &mut terminal, app, tick_rate);
 
     disable_raw_mode()?;
     execute!(
@@ -102,6 +112,8 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn run_app<B: Backend>(
+    runtime: &Runtime,
+    bus: &futures::channel::mpsc::UnboundedSender<Msg>,
     terminal: &mut Terminal<B>,
     mut app: App,
     tick_rate: Duration,
@@ -109,7 +121,7 @@ fn run_app<B: Backend>(
     let mut last_tick = Instant::now();
 
     loop {
-        terminal.draw(|f| ui(f, &mut app))?;
+        terminal.draw(|f| ui(runtime, bus, f, &mut app))?;
 
         let timeout = tick_rate
             .checked_sub(last_tick.elapsed())
@@ -132,39 +144,51 @@ fn run_app<B: Backend>(
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+fn ui<B: Backend>(
+    runtime: &Runtime,
+    bus: &futures::channel::mpsc::UnboundedSender<Msg>,
+    f: &mut Frame<B>,
+    app: &mut App,
+) {
     let size = f.size();
 
     let titles = app
         .titles
         .iter()
-        .map(|t| Spans::from(vec![Span::styled(*t, Style::default().fg(Color::Green))]))
+        .map(|t| {
+            Spans::from(vec![Span::styled(
+                *t,
+                Style::default().fg(Color::LightGreen),
+            )])
+        })
         .collect();
 
     let tabs = Tabs::new(titles)
         .block(
             Block::default()
                 .borders(Borders::ALL)
+                .style(Style::default().bg(Color::DarkGray))
                 .title("EventStoreDB Administration Tool")
                 .title_alignment(tui::layout::Alignment::Right),
         )
         .select(app.index)
-        .style(Style::default().fg(Color::Black))
-        .highlight_style(
-            Style::default()
-                .add_modifier(Modifier::BOLD)
-                .bg(Color::White),
-        );
+        .style(Style::default().fg(Color::Gray))
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD | Modifier::REVERSED));
 
     f.render_widget(tabs, size);
 
     match app.index {
-        0 => ui_dashboard(f, app),
+        0 => ui_dashboard(runtime, bus, f, app),
         _ => {}
     }
 }
 
-fn ui_dashboard<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+fn ui_dashboard<B: Backend>(
+    runtime: &Runtime,
+    bus: &futures::channel::mpsc::UnboundedSender<Msg>,
+    f: &mut Frame<B>,
+    app: &mut App,
+) {
     let rects = Layout::default()
         .constraints([Constraint::Percentage(90)].as_ref())
         .margin(3)
@@ -175,7 +199,9 @@ fn ui_dashboard<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let header_cells = app
         .dashboard_headers
         .iter()
-        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Green)));
+
+    let state = ask_snapshot(runtime, bus.clone());
 
     let rows: Vec<Row> = Vec::new();
 
@@ -204,4 +230,145 @@ fn ui_dashboard<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         ]);
 
     f.render_stateful_widget(table, rects[0], &mut app.dashboard_table_state)
+}
+
+enum Msg {
+    Tick,
+    Snapshot(futures::channel::oneshot::Sender<State>),
+}
+
+#[derive(Default, Clone, Debug)]
+struct State {
+    stats: Stats,
+    last_created_streams: Vec<String>,
+}
+
+#[derive(Default, Clone, Debug)]
+struct Queue {
+    name: String,
+    avg_items_per_second: String,
+    length_current_try_peak: String,
+    current_idle_time: String,
+    length: String,
+    group_name: String,
+    length_lifetime_peak: String,
+    last_processed_message: String,
+    total_items_processed: String,
+    idle_time_percent: String,
+    queue_name: String,
+}
+
+#[derive(Default, Clone, Debug)]
+struct Stats {
+    queues: HashMap<String, Queue>,
+}
+
+impl Stats {
+    fn from(map: HashMap<String, String>) -> Self {
+        let mut queues = HashMap::<String, Queue>::new();
+
+        for (key, value) in map {
+            let sections = key.split('-').collect::<Vec<&str>>();
+            if sections[1] == "queue" {
+                let queue_name = sections[2].to_string();
+                let queue = queues.entry(queue_name).or_default();
+
+                match sections[3] {
+                    "idleTimePercent" => queue.idle_time_percent = value,
+                    "queueName" => queue.queue_name = value,
+                    "avgItemsPerSecond" => queue.avg_items_per_second = value,
+                    "lengthCurrentTryPeak" => queue.length_current_try_peak = value,
+                    "currentIdleTime" => queue.current_idle_time = value,
+                    "length" => queue.length = value,
+                    "groupName" => queue.group_name = value,
+                    "lengthLifetimePeak" => queue.length_lifetime_peak = value,
+                    _ => {}
+                }
+            }
+        }
+
+        Self { queues }
+    }
+}
+
+async fn ticking_loop(mut sender: futures::channel::mpsc::UnboundedSender<Msg>) {
+    let mut interval = tokio::time::interval(Duration::from_secs(2));
+    loop {
+        interval.tick().await;
+
+        if let Err(_) = sender.send(Msg::Tick).await {
+            break;
+        }
+    }
+}
+
+async fn main_esdb_loop(
+    setts: ClientSettings,
+    mut stream: futures::channel::mpsc::UnboundedReceiver<Msg>,
+) -> eventstore::Result<()> {
+    let client = eventstore::Client::new(setts.clone())?;
+    let op_client = eventstore::operations::Client::new(setts);
+    let mut state = State::default();
+    let mut stats = op_client
+        .stats(Duration::from_secs(1), true, &Default::default())
+        .await?;
+
+    let last_created_streams_options = eventstore::ReadStreamOptions::default()
+        .max_count(20)
+        .position(StreamPosition::End)
+        .backwards();
+
+    while let Some(msg) = stream.next().await {
+        match msg {
+            Msg::Snapshot(result) => {
+                let _ = result.send(state.clone());
+            }
+            Msg::Tick => {
+                state.stats = Stats::from(stats.next().await?.unwrap_or_default());
+                let mut stream_names = client
+                    .read_stream("$streams", &last_created_streams_options)
+                    .await?;
+
+                let mut streams = Vec::with_capacity(20);
+                while let Some(event) = read_stream_next(&mut stream_names).await? {
+                    let stream_name = std::str::from_utf8(event.get_original_event().data.as_ref())
+                        .expect("UTF-8 formatted text")
+                        .split('@')
+                        .collect::<Vec<&str>>()[0];
+
+                    streams.push(stream_name.to_string());
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn read_stream_next(
+    stream: &mut eventstore::ReadStream,
+) -> eventstore::Result<Option<eventstore::ResolvedEvent>> {
+    match stream.next().await {
+        Err(e) => {
+            if let eventstore::Error::ResourceNotFound = e {
+                return Ok(None);
+            }
+
+            Err(e)
+        }
+        Ok(v) => Ok(v),
+    }
+}
+
+fn ask_snapshot(runtime: &Runtime, bus: futures::channel::mpsc::UnboundedSender<Msg>) -> State {
+    let (sender, recv) = futures::channel::oneshot::channel();
+    send_msg(runtime, bus, Msg::Snapshot(sender));
+
+    runtime.block_on(recv).unwrap()
+}
+
+fn send_msg(runtime: &Runtime, mut sender: futures::channel::mpsc::UnboundedSender<Msg>, msg: Msg) {
+    runtime.spawn(async move {
+        let _ = sender.send(msg).await;
+    });
 }
